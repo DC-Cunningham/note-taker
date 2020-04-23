2886b4324e71dcf4c658c7bf90a871b7fc1dbad6
DC <dc@revolutionresearch.com.au>
2020-04-23 23:43:00 +1000

    build of get and post functions

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,12 +1,41 @@
 const path = require("path");
-const fs = reuire("fs");
+const fs = require("fs");
+
+const noteData = require("../db/db.json");
 
 module.exports = function (app) {
   app.get("/api/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "../db/db.json"));
   });
 
-  app.post("/api/notes", function (req, res) {});
+  app.post("/api/notes", function (req, res) {
+    newNote = req.body;
+    newNote.id = noteData.length + 1;
+    noteData.push(newNote);
+    fs.writeFile("./db/db.json", JSON.stringify(noteData), function (
+      err,
+      data
+    ) {
+      if (err) {
+        return console.log(err);
+      }
+      console.log("New Note Written to Database");
+    });
+  });
 
-  app.post("/api/notes/:id", function (req, res) {});
+  app.delete("/api/notes/:id", function (req, res) {
+    const noteID = req.params.id;
+    const removeNote = noteData.filter(
+      (note) => note !== (noteData.id === noteID)
+    );
+    fs.writeFile("./db/db.json", JSON.stringify(removeNote), function (
+      err,
+      data
+    ) {
+      if (err) {
+        return console.log(err);
+      }
+      console.log("Note deleted from Database");
+    });
+  });
 };
