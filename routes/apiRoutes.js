38fbda86a5c9e8cac587ed5335562367812a6507
DC <dc@revolutionresearch.com.au>
2020-04-25 18:29:44 +1000

    fixed delete path and script

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,41 +1,42 @@
 const path = require("path");
 const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
 
 const noteData = require("../db/db.json");
 
-module.exports = function (app) {
-  app.get("/api/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "../db/db.json"));
-  });
+module.exports = (app) => {
+  app.get("/api/notes", (req, res) =>
+    res.sendFile(path.join(__dirname, "../db/db.json"))
+  );
 
-  app.post("/api/notes", function (req, res) {
-    newNote = req.body;
-    newNote.id = noteData.length + 1;
-    noteData.push(newNote);
-    fs.writeFile("./db/db.json", JSON.stringify(noteData), function (
-      err,
-      data
-    ) {
+  app.post("/api/notes", (req, res) => {
+    const { body: newNote } = req;
+    noteData.push({ ...newNote, id: uuidv4() });
+    fs.writeFile("../db/db.json", JSON.stringify(noteData, null, 2), (err) => {
       if (err) {
         return console.log(err);
       }
       console.log("New Note Written to Database");
+      res.json({ added: true });
     });
   });
 
-  app.delete("/api/notes/:id", function (req, res) {
-    const noteID = req.params.id;
-    const removeNote = noteData.filter(
-      (note) => note !== (noteData.id === noteID)
-    );
-    fs.writeFile("./db/db.json", JSON.stringify(removeNote), function (
-      err,
-      data
-    ) {
-      if (err) {
-        return console.log(err);
+  app.delete("/api/notes/:id", (req, res) => {
+    const {
+      params: { id: noteId },
+    } = req;
+
+    const filteredArray = noteData.filter((note) => note.id != noteId);
+    fs.writeFile(
+      "db/db.json",
+      JSON.stringify(filteredArray, null, 2),
+      (err) => {
+        if (err) {
+          throw err;
+        }
+        console.log("Note deleted from Database");
+        res.json({ deleted: true });
       }
-      console.log("Note deleted from Database");
-    });
+    );
   });
 };
